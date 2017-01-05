60f774850f0b86cb898b75d1f4951e44b6025a2f
uwant <uwant@jinpingguodemacbook-pro.local>
2017-01-05 12:37:56 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by uwant on 17/1/5.
+//  Copyright © 2017年 luojing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
